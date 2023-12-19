3d430ba5ed04933e829bd767cf11b2d5712349f1
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2023-12-19 11:42:59 +0300

    Added AI script

---
+++ AI_cloud_script.sh
@@ -0,0 +1,20 @@
+#This script is to install AI models based on a git hub repository below.
+#The repository link is as follows https://github.com/jmorganca/ollama
+
+
+#!/bin/bash
+#change to sudo
+sudo -i
+#Update the system
+apt-get update 
+#or for yum use 
+yum update
+
+#Install the script
+curl https://ollama.ai/install.sh | sh
+
+#Run the models
+ollama run --model=gpt-3.5-turbo --prompt="What is the capital of France?" #Example 1
+
+ollama run llama2 #Example 2 
+

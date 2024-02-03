1ca2e07297e668f7324907e3e58c82c9d6568cb0
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-03 23:13:56 +0300

    Feat:Added comments for easy readability

--- rust_ui.sh
+++ rust_ui.sh
@@ -4,3 +4,9 @@ echo "Enter the name for a new project"
 read input
 
 cargo generate --git https://github.com/slint-ui/slint-rust-template --name $input
+
+
+#To make this script available anywhere add the following line to your .bashrc file
+#export PATH=$PATH:/path/to/your/script
+#Replace /path/to/your/script with the path to your script
+#Then you can run the script from anywhere by typing the script name in the terminal

0994e5bb07c102e6b6cd06bb63b7f00aeeeb769e
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-18 22:00:57 +0300

    Feat:Added sleep

--- new.sh
+++ new.sh
@@ -3,9 +3,11 @@
 echo "Creating a new user"
 sleep 2
 echo "Enter the username"
+sleep 3
 read newuser
 sudo adduser $newuser
 sudo usermod -aG sudo newuser
 echo "Done, new user created"
+
 # Path: update.sh
 # This simple script is supposed to update the system and clean unused packages

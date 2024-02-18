8fb6eae5d3be35e2a72660318d34d181258bd1f8
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-18 22:01:38 +0300

    Feat:Added ability for user passwrd
    I made it so you can change the user password

--- new.sh
+++ new.sh
@@ -8,6 +8,9 @@ read newuser
 sudo adduser $newuser
 sudo usermod -aG sudo newuser
 echo "Done, new user created"
-
+#Change the passowrd for the new user
+echo "Changing the password for the new user"
+sudo passwd $newuser
+echo "Done, password changed"
 # Path: update.sh
 # This simple script is supposed to update the system and clean unused packages

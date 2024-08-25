3194b03bcd96b72710d7ef8d4e40b66e06a2a6d3
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-08-26 00:48:05 +0300

    Feat:Made changes

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 #This simple script is supposed to update the system and clean unused packages
 #!/bin/bash
 echo "Updating the system"
+sleep 5
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove -y
 sudo apt clean -y
 sudo apt autoclean -y
+sleep 6
 echo "Done,Enjoy your updated system!"
